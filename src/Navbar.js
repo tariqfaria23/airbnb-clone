e26a3568e9a617130a2a3313bb17550a8a480c01
Faria Tariq <76790237+tariqfaria23@users.noreply.github.com>
2023-08-31 10:31:54 +0530

    final commit

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import img from "./images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={img}/>
+        </nav>
+    )
+}
